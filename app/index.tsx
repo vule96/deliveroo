b9b53fa47e18ecbdd0bc48e7a630a68caf50e700
vule <vulehuy2905@gmail.com>
2024-07-18 11:11:16 +0700

    feat: handle add details and ParallaxScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Categories from "../components/Categories";
+import Restaurants from "../components/Restaurants";
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    top: 50,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
 
 export default Page;
